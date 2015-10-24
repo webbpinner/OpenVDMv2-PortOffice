6d182e51beac46060f9305647bc0a41c1b6b4970
Webb Pinner <webbpinner@gmail.com>
2015-10-24 08:02:26 -0400

    Updated files to simplify installtion.

--- OpenVDMv2-PO_db.sql
+++ OpenVDMv2-PO_db.sql
@@ -37,8 +37,8 @@ LOCK TABLES `ovdmpo_CoreVars` WRITE;
 
 INSERT INTO `ovdmpo_CoreVars` (`coreVarID`, `name`, `value`)
 VALUES
-	(1,'shoresideDWBaseDir','/mnt/vault/shoreside'),
-	(2,'shoresideDWApacheDir','/Shoreside');
+	(1,'shoresideDWBaseDir','/mnt/vault/CruiseData'),
+	(2,'shoresideDWApacheDir','/CruiseData');
 
 /*!40000 ALTER TABLE `ovdmpo_CoreVars` ENABLE KEYS */;
 UNLOCK TABLES;
